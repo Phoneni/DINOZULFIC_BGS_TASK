672ed50e3bdf900d1153722428edc8ecf7efdf14
Phoneni <zulficdino@hotmail.com>
2024-05-27 17:31:28 +0200

    Removed unused imported assets

---
+++ Source/DinoZulfic_BGS_Task/SkateGameState.cpp
@@ -0,0 +1,4 @@
+
+
+#include "SkateGameState.h"
+
---
+++ Source/DinoZulfic_BGS_Task/SkateGameState.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "SkateGameState.generated.h"
+
+
+UCLASS()
+class DINOZULFIC_BGS_TASK_API ASkateGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
